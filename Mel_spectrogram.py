6fb16ccebd4583a4912e8dad02b8bc716d6616c6
Sam <70551337+samcyx@users.noreply.github.com>
2025-02-26 12:04:14 -0500

    removed communication functionality

--- Mel_spectrogram.py
+++ Mel_spectrogram.py
@@ -1,31 +1,30 @@
 import os
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
+import serial
 
 def show_mel_bands(filter_banks, source):
-    plt.figure(figsize=(25,10))
-    librosa.display.specshow(filter_banks,sr = source, x_axis = "linear")
+    plt.figure(figsize = (25, 10))
+    librosa.display.specshow(filter_banks, sr = source, x_axis = "linear")
     plt.colorbar(format="%+2.f")
     plt.show()
 
 def show_mel_spectrogram(logarithmic_spectrogram, sr):
-    plt.figure(figsize = (25,10))
-    librosa.display.specshow(logarithmic_spectrogram, x_axis = "time", y_axis = "mel",sr = sr)
+    plt.figure(figsize = (25, 10))
+    librosa.display.specshow(logarithmic_spectrogram, x_axis = "time", y_axis = "mel", sr = sr)
     plt.colorbar(format = "%+2.f")
     plt.show()
 
+# Load audio file
 BASE_DIR = "/Users/samcy/OneDrive - University of Waterloo/Harmonic-Forest/Music-Assets"
-
 file_name = "25481__freqman__violin-minuet_boccherini-edit.wav"
 file_path = os.path.join(BASE_DIR, file_name)
 y, sr = librosa.load(file_path)
-filter_banks = librosa.filters.mel(n_fft = 2048, sr = 22050, n_mels = 10)
-#show_mel_bands(filter_banks, sr)
 
-mel_spectrogram = librosa.feature.melspectrogram(y = y, sr = sr, n_fft = 2048, hop_length = 512, n_mels = 200)
-
-
+# Generate mel spectrogram
+N_MELS = 6  # Number of frequency bands, should match with # of rods...
+mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=512, n_mels=N_MELS)
 log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
 
-show_mel_spectrogram(log_mel_spectrogram, sr)
