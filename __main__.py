9187c369407a61de11c488dabe37b6513a1ac819
Sam <70551337+samcyx@users.noreply.github.com>
2025-02-27 21:01:09 -0500

    entry point

---
+++ __main__.py
@@ -0,0 +1,8 @@
+import extract_frequencies
+import os
+BASE_DIR = "/Users/samcy/OneDrive - University of Waterloo/Harmonic-Forest/Music-Assets"
+file_name = "just-major-sc.wav"
+file_path = os.path.join(BASE_DIR, file_name)
+
+normalized_activations, bucket_edges = extract_frequencies.analyze_frequency_buckets(file_path)
+extract_frequencies.visualize_activations(normalized_activations,bucket_edges)
