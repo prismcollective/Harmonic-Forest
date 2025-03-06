f7d30f4fd417a7486503373f45f7a7b96101cc48
Sam <70551337+samcyx@users.noreply.github.com>
2025-03-06 13:42:26 -0500

    reflects new class

--- __main__.py
+++ __main__.py
@@ -1,8 +1,15 @@
-import extract_frequencies
+import audio_file
 import audio_to_arduino
 import numpy as np
-normalized_activations = np.load('normalized-activations.npy')
-bucket_edges = np.load('bucket-edges.npy')
+import time
 
-extract_frequencies.visualize_activations(normalized_activations,bucket_edges)
-audio_to_arduino.send_to_arduino(normalized_activations)
+violin = audio_file.audio_file("violin-c4.wav")
+major_scale = audio_file.audio_file("just-major-sc.wav")
+minuet_bocc = audio_file.audio_file("25481__freqman__violin-minuet_boccherini-edit.wav")
+
+timer = time.time()
+
+audio_to_arduino.send_to_arduino(minuet_bocc.get_normalized_activations(), minuet_bocc.get_audio_duration())
+print(minuet_bocc.get_audio_duration()/minuet_bocc.get_normalized_activations().shape[1] * minuet_bocc.get_normalized_activations().shape[1])
+print("End Time:", time.time()-timer)
+#extract_frequencies.visualize_activations(normalized_activations,bucket_edges)
